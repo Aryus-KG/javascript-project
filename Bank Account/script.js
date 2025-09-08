4d8525843d08d3a7d93b997d7a4223605e291f00
Phntme <aryushere@gmail.com>
2025-09-08 18:40:32 +0700

    feature update
    > mengganti user aryus dengan kageshi
    > menambahkan user topaz
    > menambahkan fitur history
    > menambahkan fitur transfer
    -----------------------------------
    > History
    Gunakan .history untuk melihat riwayat yang telah dilakukan oleh user. dengan .history kamu bisa melihat aktivitas deposit, transfer dan withdraw dari user.
    
    > Transfer
    Gunakan .transfer(jumlah, target) untuk melakukan transfer ke user lain (jika saldomu tidak cukup maka tidak bisa transfer). (jumlah) adalah berapa banyak saldo yang ingin kamu kirim, sementara (target) adalah user yang hendak kamu kirimi saldo.
    
    contoh:
    
    kageshi.transfer(100, topaz)
    > saldo kageshi - 100
    > saldo topaz + 100

--- Bank Account/script.js
+++ Bank Account/script.js
@@ -2,13 +2,21 @@ class Account {
   constructor(name, balance = 0) {
     this.name = name;
     this.balance = balance;
+    this.history = [];
   }
+
+  //   class Bank {
+  //     constructor(){
+
+  //     }
+  //   }
 
   deposit(amount) {
     if (amount <= 0) {
       console.log(`harap masukkan jumlah yang valid!`);
     } else {
       this.balance += amount;
+      this.history.push(`deposit sebanyak ${amount}`);
     }
   }
 
@@ -19,8 +27,23 @@ class Account {
       this.balance -= amount;
       console.log(`anda berhasil melakukan withdraw sebanyak ${amount}
 saldo anda tersisa ${this.balance}`);
+      this.history.push(`withdraw ${amount}`);
+    }
+  }
+
+  transfer(amount, target) {
+    if (this.balance < amount) {
+      console.log(`bro.. lu aja gada duit mau sosoan transfer wakakak`);
+    } else {
+      target.deposit(amount);
+      this.balance -= amount;
+      console.log(
+        `kamu berhasil transfer sebesar ${amount} kepada ${target.name}`
+      );
+      this.history.push(`transfer sebesar ${amount} kepada ${target.name}`);
     }
   }
 }
 
-let akun1 = new Account("aryus");
+let kageshi = new Account("Kageshi");
+let topaz = new Account("Topaz", 1000000000000000);
